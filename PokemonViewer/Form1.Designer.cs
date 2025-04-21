0376dfb39d25b1a786a34e001640350419f5eeb3
Matthew Berumen <mberumen2005@gmail.com>
2025-04-21 16:38:19 -0400

    maded the form design

--- PokemonViewer/Form1.Designer.cs
+++ PokemonViewer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PokemonViewer
 {
-    partial class Form1
+    partial class Name
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPokemonName = new System.Windows.Forms.TextBox();
+            this.btnGetPokemon = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblHappiness = new System.Windows.Forms.Label();
+            this.lblCaptureRate = new System.Windows.Forms.Label();
+            this.lblHabitat = new System.Windows.Forms.Label();
+            this.lblGrowthRate = new System.Windows.Forms.Label();
+            this.lblFlavorText = new System.Windows.Forms.Label();
+            this.lblEggGroup = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPokemonName
+            // 
+            this.txtPokemonName.Location = new System.Drawing.Point(93, 108);
+            this.txtPokemonName.Name = "txtPokemonName";
+            this.txtPokemonName.Size = new System.Drawing.Size(100, 20);
+            this.txtPokemonName.TabIndex = 0;
+            // 
+            // btnGetPokemon
+            // 
+            this.btnGetPokemon.Location = new System.Drawing.Point(240, 108);
+            this.btnGetPokemon.Name = "btnGetPokemon";
+            this.btnGetPokemon.Size = new System.Drawing.Size(75, 23);
+            this.btnGetPokemon.TabIndex = 1;
+            this.btnGetPokemon.Text = "button1";
+            this.btnGetPokemon.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(175, 167);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "label1";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblHappiness
+            // 
+            this.lblHappiness.AutoSize = true;
+            this.lblHappiness.Location = new System.Drawing.Point(175, 199);
+            this.lblHappiness.Name = "lblHappiness";
+            this.lblHappiness.Size = new System.Drawing.Size(35, 13);
+            this.lblHappiness.TabIndex = 3;
+            this.lblHappiness.Text = "label2";
+            // 
+            // lblCaptureRate
+            // 
+            this.lblCaptureRate.AutoSize = true;
+            this.lblCaptureRate.Location = new System.Drawing.Point(175, 231);
+            this.lblCaptureRate.Name = "lblCaptureRate";
+            this.lblCaptureRate.Size = new System.Drawing.Size(35, 13);
+            this.lblCaptureRate.TabIndex = 4;
+            this.lblCaptureRate.Text = "label3";
+            // 
+            // lblHabitat
+            // 
+            this.lblHabitat.AutoSize = true;
+            this.lblHabitat.Location = new System.Drawing.Point(175, 261);
+            this.lblHabitat.Name = "lblHabitat";
+            this.lblHabitat.Size = new System.Drawing.Size(35, 13);
+            this.lblHabitat.TabIndex = 5;
+            this.lblHabitat.Text = "label4";
+            // 
+            // lblGrowthRate
+            // 
+            this.lblGrowthRate.AutoSize = true;
+            this.lblGrowthRate.Location = new System.Drawing.Point(175, 287);
+            this.lblGrowthRate.Name = "lblGrowthRate";
+            this.lblGrowthRate.Size = new System.Drawing.Size(35, 13);
+            this.lblGrowthRate.TabIndex = 6;
+            this.lblGrowthRate.Text = "label5";
+            // 
+            // lblFlavorText
+            // 
+            this.lblFlavorText.AutoSize = true;
+            this.lblFlavorText.Location = new System.Drawing.Point(175, 315);
+            this.lblFlavorText.Name = "lblFlavorText";
+            this.lblFlavorText.Size = new System.Drawing.Size(35, 13);
+            this.lblFlavorText.TabIndex = 7;
+            this.lblFlavorText.Text = "label6";
+            // 
+            // lblEggGroup
+            // 
+            this.lblEggGroup.AutoSize = true;
+            this.lblEggGroup.Location = new System.Drawing.Point(175, 340);
+            this.lblEggGroup.Name = "lblEggGroup";
+            this.lblEggGroup.Size = new System.Drawing.Size(35, 13);
+            this.lblEggGroup.TabIndex = 8;
+            this.lblEggGroup.Text = "label7";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Base Happines";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Capture Rate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(90, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Habitat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(90, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Growth Rate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(90, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Flavor Text";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(90, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Egg Group";
+            // 
+            // Name
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1254, 607);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblEggGroup);
+            this.Controls.Add(this.lblFlavorText);
+            this.Controls.Add(this.lblGrowthRate);
+            this.Controls.Add(this.lblHabitat);
+            this.Controls.Add(this.lblCaptureRate);
+            this.Controls.Add(this.lblHappiness);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnGetPokemon);
+            this.Controls.Add(this.txtPokemonName);
+            this.Name = "Name";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPokemonName;
+        private System.Windows.Forms.Button btnGetPokemon;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblHappiness;
+        private System.Windows.Forms.Label lblCaptureRate;
+        private System.Windows.Forms.Label lblHabitat;
+        private System.Windows.Forms.Label lblGrowthRate;
+        private System.Windows.Forms.Label lblFlavorText;
+        private System.Windows.Forms.Label lblEggGroup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- PokemonViewer/Form1.cs
+++ PokemonViewer/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace PokemonViewer
 {
-    public partial class Form1 : Form
+    public partial class Name : Form
     {
-        public Form1()
+        public Name()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
